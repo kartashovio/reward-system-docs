0d33eaa706dc2250352e7e81671ca9ea7633387f
AlphaBaqpla <str14017355@gmail.com>
2023-01-06 15:23:10 +0300

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1,13 +1,13 @@
 function themeToggle(){
     let themeCss = document.querySelector(".theme");
     if(themeCss !== null){
-        if(!window.isDocsifyDarkMode) {
-            window.isDocsifyDarkMode = true;
+        if(!window.isDocsifyLightMode) {
+            window.isDocsifyLightMode = true;
             themeCss.setAttribute("href", "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css");
             updateCustomCSS();
         }
         else{
-            window.isDocsifyDarkMode = false;
+            window.isDocsifyLightMode = false;
             themeCss.setAttribute("href", "https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple-dark.css");
             updateCustomCSS();
         }
@@ -18,7 +18,7 @@ function updateCustomCSS() {
     let tableBars = document.querySelectorAll('.markdown-section tr:nth-child(2n)');
     if (tableBars !== null) {
         for (var i = 0; i < tableBars.length; ++i)
-        tableBars[i].style.backgroundColor = window.isDocsifyDarkMode ? "#363636" : "#f8f8f8";
+        tableBars[i].style.backgroundColor = window.isDocsifyLightMode ? "#f8f8f8" : "#363636";
     }
 }
 window.updateCustomCSS = updateCustomCSS;
